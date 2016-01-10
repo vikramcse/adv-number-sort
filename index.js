eae9edb767d72135d7b532ade28bdad50c7550e9
vikramcse <vikramcse.10@gmail.com>
2016-01-10 22:08:51 +0530

    Added implementation

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(arr, option) {
+	var asc = function(a, b) {
+		return a - b;
+	};
+
+	var desc = function(a, b) {
+		return b - a;
+	};
+
+	if (arr) {
+		if (option && option != 'undefined' && option === 'desc')
+			return arr.sort(desc);
+		return arr.sort(asc);
+	}
+};
