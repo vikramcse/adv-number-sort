69cbfa8322c62d82dbd91e729ea49d7cd3a633ea
vikramcse <vikramcse.10@gmail.com>
2016-01-10 22:09:02 +0530

    Added tests for implementation

---
+++ test.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect;
+var advSort = require('./index.js');
+
+describe('tests for array sorting', function() {
+	it('check for descending output', function() {
+		var arr = advSort([1, 2, 3, 4], "desc");
+		expect(arr).to.eql([4, 3, 2, 1]);
+	});
+
+	it('check for ascending output', function() {
+		var arr = advSort([1, 2, 3, 4], "asc");
+		expect(arr).to.eql([1, 2, 3, 4]);
+	});
+
+	it('check for ascending output without options', function() {
+		var arr = advSort([1, 2, 3, 4]);
+		expect(arr).to.eql([1, 2, 3, 4]);
+	});
+});
