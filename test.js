1a84fff628ba3308bc7ffead588c5319d6b06c81
vikramcse <vikramcse.10@gmail.com>
2016-02-27 20:41:37 +0530

    Changes in test

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 var expect = require('chai').expect;
 var advSort = require('./index.js');
 
-describe('tests for array sorting', function() {
-	it('check for descending output', function() {
-		var arr = advSort([1, 2, 3, 4], "desc");
-		expect(arr).to.eql([4, 3, 2, 1]);
-	});
+describe('tests for array sorting', function () {
+  it('check for descending output', function () {
+    var arr = advSort([1, 2, 3, 4], 'desc');
+    expect(arr).to.eql([4, 3, 2, 1]);
+  });
 
-	it('check for ascending output', function() {
-		var arr = advSort([1, 2, 3, 4], "asc");
-		expect(arr).to.eql([1, 2, 3, 4]);
-	});
+  it('check for ascending output', function () {
+    var arr = advSort([4, 3, 2, 1], 'asc');
+    expect(arr).to.eql([1, 2, 3, 4]);
+  });
 
-	it('check for ascending output without options', function() {
-		var arr = advSort([1, 2, 3, 4]);
-		expect(arr).to.eql([1, 2, 3, 4]);
-	});
+  it('check for ascending output without options', function () {
+    var arr = advSort([1, 2, 3, 4]);
+    expect(arr).to.eql([1, 2, 3, 4]);
+  });
 });
